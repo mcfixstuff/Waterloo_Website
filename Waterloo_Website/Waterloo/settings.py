ef6f6c3aed0adaea3fb170ef36e76769ecacec00
mcfixstuff <mcfixstuff2@gmail.com>
2025-04-21 11:57:40 -0500

    Making it easier to understand

--- Waterloo_Website/Waterloo/settings.py
+++ Waterloo_Website/Waterloo/settings.py
@@ -18,10 +18,6 @@ from decouple import config, Csv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-nd(^d3x6oo0)s1f78_j+n!pmxw02y0zax-_&+0x4gb(91u5*b^')
 
